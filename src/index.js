e863ab12eb405d69c9eff6988a8b4b45b549adf8
Andris Reinman <andris@kreata.ee>
2019-11-09 23:15:58 +0200

    handle rename and delete project menu items

--- src/index.js
+++ src/index.js
@@ -63,6 +63,7 @@ const createWindow = () => {
         // in an array if your app supports multi windows, this is the time
         // when you should delete the corresponding element.
         mainWindow = null;
+        app.quit();
     });
 
     mainWindow.on('blur', () => {
