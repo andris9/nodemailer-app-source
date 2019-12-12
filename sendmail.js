b961834d815cc7c8374cd84de88d0d3d54291c25
Andris Reinman <andris@kreata.ee>
2019-12-12 10:53:06 +0200

    bumped deps

--- sendmail.js
+++ sendmail.js
@@ -1,5 +1,9 @@
 'use strict';
 
+// allows to resolve homedir correctly using effective UID instead of using $HOME
+// otherwise suid does not work
+delete process.env.HOME;
+
 const { getAppDataPath } = require('appdata-path');
 const cli = require('./src/cli/cli');
 
